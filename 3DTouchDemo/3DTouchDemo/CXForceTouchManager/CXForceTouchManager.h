8b5d5a5e685cee519ff42f8ac5a5c389febc791f
chengxuzheng <zheng890715@gmail.com>
2017-04-30 21:15:41 +0800

    param annotation

--- 3DTouchDemo/3DTouchDemo/CXForceTouchManager/CXForceTouchManager.h
+++ 3DTouchDemo/3DTouchDemo/CXForceTouchManager/CXForceTouchManager.h
@@ -18,15 +18,20 @@
 + (void)makeShortcutItems:(void(^_Nonnull)(CXForceTouchManager *_Nonnull manager))block;
 
 
-/** 添加item方法 **/
+/** 添加item的方法
+    type 通知标识符
+    title 标题
+    subtitle 副标题
+    imageName 图片名
+ */
 - (CXForceTouchManager *_Nonnull(^_Nonnull)(NSString * _Nonnull type, NSString * _Nonnull title, NSString * _Nullable subtitle, NSString * _Nonnull imageName))addItem;
 
 
-/** 完成添加item方法 **/
+/** 完成添加item的方法 **/
 - (CXForceTouchManager * _Nonnull (^_Nonnull)())finished;
 
 
-/** 点击item方法 **/
+/** 点击item的方法 **/
 + (void)didSelectItemWithType:(NSString *_Nonnull)type;
 
 @end
